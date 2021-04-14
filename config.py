86f721f3392e952f91586c0b9dba3df622b4c18f
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-04-14 12:03:02 -0300

    test: try the script out with another movie

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """Target URLs to scrape."""
 
-URLS = [ 'https://www.rottentomatoes.com/m/wonder_woman_1984',
-        'https://www.imdb.com/title/tt7126948/',
-        'https://www.metacritic.com/movie/wonder-woman-1984',
-        'https://www.ecartelera.com/peliculas/wonder-woman-1984', ]
+URLS = [ 'https://www.rottentomatoes.com/m/monty_python_and_the_holy_grail',
+        'https://www.imdb.com/title/tt0071853/',
+        'https://www.metacritic.com/movie/monty-python-and-the-holy-grail',
+        'https://www.ecartelera.com/peliculas/los-caballeros-de-la-mesa-cuadrada-y-sus-locos-seguidores/', ]
